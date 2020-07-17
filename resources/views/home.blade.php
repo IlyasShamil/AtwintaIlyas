36137beef47ad9f9fd4eaaaebc370185e6e119ed
IlyasShamil <danielo.rafoo@gmail.com>
2020-07-17 14:30:15 +0700

    Create Models

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are logged in!') }} {{ Auth::user()->roles }}<!-- вызов имени юзера  -->
                 </div>
             </div>
         </div>

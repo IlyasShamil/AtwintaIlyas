c28679a0856777e9d21930b32ccd3c405b46ff71
IlyasShamil <danielo.rafoo@gmail.com>
2020-08-03 11:04:54 +0700

    добавление проверки пуша

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,6 +66,7 @@
     <body>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
+            <li><br></li>
                 <div class="top-right links">
                     @auth
                         
